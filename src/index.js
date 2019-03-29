96fe5b19899a23750c8ddccb6c109873068715cf
Fabián Jaramillo <fabiancoj@hotmail.com>
2019-03-28 20:07:04 -0500

    se crea un api de tareas, se crea la coneccion a la base de datos en mongo, se agregan las dependencias de morgan, mongoose y nodemon

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 const app = express();
+mongoose.connect('mongodb://localhost/mevn')
+    .then(db => console.log('BD esta conectada'))
+    .catch(err => console.error(err))
 
 //Setting
 app.set('port', process.env.PORT || 3000)
 
 //Middlewares
+app.use(morgan('dev'));
+app.use(express.json())
 
 //Routes
+app.use('/tasks', require('./routes/tasks'))
 
 //Static files
 app.use(express.static(__dirname+'/public'))
---
+++ src/routes/tasks.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router();
+
+const Task = require('../models/Task')
+
+router.get('/', async (req, res) => {
+    const tasks = await Task.find()
+    console.log(tasks)
+    res.json(tasks)
+})
+
+router.post('/', async (req,res) => {
+    const task = new Task(req.body)
+    await task.save()
+    res.json({
+        status : 'tarea guardada'
+    })
+    //console.log(req.body)
+})
+
+router.put('/:id', async (req, res) => {
+    console.log(req.params.id)
+    await Task.findByIdAndUpdate(req.params.id, req.body)
+    res.json({
+        status: 'Task router'
+    })
+})
+
+router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id)
+    res.json({
+        status: 'Task router'
+    })
+})
+
+module.exports = router
+
